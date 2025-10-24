4ad1d27d30b6f4e3600b129b16578cf0e3260e96
nikcevichlucas-max <tu_email@gmail.com>
2025-10-24 16:57:54 -0300

    fix: render port configuration

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,11 @@
 import reflex as rx
+import os
+
+class Config(rx.Config):
+    pass
 
 config = rx.Config(
     app_name="portafolio",
-    plugins=[
-        rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
-    ]
+    api_port=int(os.getenv("PORT", "8000")),
+    deploy_url="https://wallet-port-oficial.onrender.com"
 )
